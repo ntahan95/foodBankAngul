163bac3935c25580abdd02f5033514c662411749
Samuel Murray <smurra28@uncc.edu>
2019-09-12 17:43:17 -0400

    Add item-detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemDetailComponent } from './item-detail/item-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemDetailComponent
   ],
   imports: [
     BrowserModule,
